1e6ecd75d6bf5c203e4c9305c9dbe27e559ae9b4
Joseph-Jnr <josephnwobodo1@gmail.com>
2024-03-20 06:56:41 +0100

    fix: build error

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,2 +1,9 @@
 // Auth.ts
-export const isAuthenticated = !!localStorage.getItem('chatterAuthToken')
+
+// Check if localStorage is available (for server-side rendering)
+const isLocalStorageAvailable =
+  typeof window !== 'undefined' && window.localStorage
+
+// Function to retrieve the bearer token from local storage
+export const isAuthenticated =
+  isLocalStorageAvailable && !!localStorage.getItem('chatterAuthToken')
